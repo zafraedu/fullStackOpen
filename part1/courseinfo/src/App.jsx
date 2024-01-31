4e98327d1754e13ea8f6d3afcd05b83447a503ff
Eduardo Zafra <duduzafra@gmail.com>
2024-01-31 23:35:52 +0100

    part1 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,13 +1,28 @@
 const Header = (props) => <h1>{props.course}</h1>;
-const Content = (props) => <p>{props.namePart} {props.numExe}</p>
-const Total = (props) => <p>Number of exercises {props.n1 + props.n2 + props.n3}</p>
 
-const App = () =>  (
+const Part = (props) => {
+	return (
+		<p>
+			{props.namePart} {props.numExe}
+		</p>
+	);
+};
+
+const Content = () => {
+	return (
+		<>
+			<Part namePart="Fundamentals of React" numExe="10" />
+			<Part namePart="Using props to pass data" numExe="7" />
+			<Part namePart="State of a component" numExe="14" />
+		</>
+	);
+};
+const Total = (props) => <p>Number of exercises {props.n1 + props.n2 + props.n3}</p>;
+
+const App = () => (
 	<div>
 		<Header course="Half Stack application development" />
-		<Content namePart="Fundamentals of React" numExe="10" />
-		<Content namePart="Using props to pass data" numExe="7" />
-		<Content namePart="State of a component" numExe="14" />
+		<Content />
 		<Total n1={10} n2={7} n3={14} />
 	</div>
 );
