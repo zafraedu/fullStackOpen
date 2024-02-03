73f2d08ff6fe89152c305c860d67a517e52b2076
Eduardo Zafra <duduzafra@gmail.com>
2024-02-04 00:56:39 +0100

    part1/unicafe 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-const Statistics = ({value, count}) => <p>{value}: {count}</p>
+const Statistics = ({value, count, pos}) => <p>{value}: {count} {pos}</p>
 
 const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+
+	const avarage = () => (good + neutral + bad) / 3;
+	const positive = () => good === 0 ? 0 : good / (good + neutral + bad) * 100;
 
 	return (
 		<div>
@@ -18,6 +21,8 @@ const App = () => {
       <Statistics value="good" count={good}/>
       <Statistics value="neutral" count={neutral}/>
       <Statistics value="bad" count={bad}/>
+      <Statistics value="avarage" count={avarage().toFixed(2)}/>
+      <Statistics value="positive" count={positive().toFixed(2)} pos="%"/>
 		</div>
 	);
 };
