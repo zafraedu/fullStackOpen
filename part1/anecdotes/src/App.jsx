a681c1bfe3c1340dc99ec1567df65139faa1b3c6
Eduardo Zafra <duduzafra@gmail.com>
2024-02-04 12:39:17 +0100

    part1/anecdotes 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
-
-const Button = ({ value, onClick }) => {
-	return <button onClick={onClick}>{value}</button>;
+//Components
+const Header = ({ h2 }) => <h2>{h2}</h2>;
+const Button = ({ value, onClick }) => <button onClick={onClick}>{value}</button>;
+const GeneralAnecdote = ({ anecdote }) => <p>{anecdote}</p>;
+const PopularAnecdote = ({ anecdote, voted }) => {
+	const highValue = Math.max(...voted);
+	if (highValue <= 0) return <p>No votes yet</p>;
+	return (
+		<>
+			<p>{anecdote[voted.indexOf(highValue)]}</p>
+			<p>has {highValue} votes</p>
+		</>
+	);
 };
-
+//Main App
 const App = () => {
 	const anecdotes = [
 		"If it hurts, do it more often.",
@@ -15,27 +25,26 @@ const App = () => {
 		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
 		"The only way to go fast, is to go well.",
 	];
-
+	//hooks
 	const [selected, setSelected] = useState(0);
-	const [voted, setVoted] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
-
-	const onGenerate = () => {
-		setSelected((anecdotes[selected] = Math.floor(Math.random() * anecdotes.length)));
-	};
-
+	const [voted, setVoted] = useState(Array(8).fill(0));
+	//handle Events
+	const onGenerate = () => setSelected((anecdotes[selected] = Math.floor(Math.random() * anecdotes.length)));
 	const onVote = () => {
-    let cpy = [...voted];
+		let cpy = [...voted];
 		cpy[selected] += 1;
-    setVoted(cpy);
-    console.log(cpy);//test
+		setVoted(cpy);
 	};
-
+	//Display
 	return (
-		<>
-			<div>{anecdotes[selected]}</div>
+		<div>
+			<Header h2="Anecdote of the day" />
+			<GeneralAnecdote anecdote={anecdotes[selected]} />
 			<Button value="vote" onClick={onVote} />
 			<Button value="next anecdote" onClick={onGenerate} />
-		</>
+			<Header h2="Anecdote with most votes" />
+			<PopularAnecdote anecdote={anecdotes} voted={voted} />
+		</div>
 	);
 };
 
