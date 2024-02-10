3d9f6192f1f814bf268e0f75aa2be894b826face
Eduardo Zafra <duduzafra@gmail.com>
2024-02-10 15:26:43 +0100

    part2: 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { H2 } from "./components/Header.jsx";
 import List from "./components/List.jsx";
 import Form, { InputChange } from "./components/Form.jsx";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [filter, setFilter] = useState("");
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3001/persons")
+			.then((response) => {
+				setPersons(response.data);
+			});
+	}, []);
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
